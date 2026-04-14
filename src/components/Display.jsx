f5220c36dafa195d64ac7eaf388f3c0b0d05668a
SeungwooDY <sdyoon0701@gmail.com>
2026-04-13 22:57:11 -0400

    icons

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -3,6 +3,10 @@ export default function Display({ lat, lon, currentWeather, units }) {
         <>
             <div className='display'>
                 <p>{units === "imperial" ? `Current Temp: ${currentWeather?.main?.temp} F` : `Current Temp: ${currentWeather?.main?.temp} C`}</p>
+                <img className="icon-display"
+                    src={`https://openweathermap.org/img/wn/${currentWeather?.weather?.[0]?.icon}@2x.png`}
+                    alt={currentWeather?.weather?.[0]?.description}
+                />
                 <p>{units === "imperial" ? `Feels Like: ${currentWeather?.main?.feels_like} F` : `Feels Like: ${currentWeather?.main?.temp} C`}</p>
                 <p>Humidity: {currentWeather?.main?.humidity}%</p>
             </div>
